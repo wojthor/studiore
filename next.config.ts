ae581060e1866efda1b18563e1195df29c1b9dca
Wojciech's Macbook Pro <wojciechsmacbookpro@macbook-pro-wojciechs.home>
2026-02-11 12:20:30 +0100

    next fix

--- next.config.ts
+++ next.config.ts
@@ -11,14 +11,7 @@ const nextConfig: NextConfig = {
 			},
 		],
 	},
-	eslint: {
-		// Ostrzeżenie: To pozwala na build projektu nawet z błędami ESLint.
-		ignoreDuringBuilds: true,
-	},
-	typescript: {
-		// Ostrzeżenie: To pozwala na build projektu nawet z błędami TypeScript.
-		ignoreBuildErrors: true,
-	},
+	
 };
 
 export default nextConfig;
